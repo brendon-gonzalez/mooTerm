9407234d75959e0ec32e03e9523083906e7d7fc7
Brendon Gonzalez <brendon@brandingbrand.com>
2015-04-22 01:49:29 -0400

    Add basic error handlng

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ var request = require('request');
 var cheerio = require('cheerio');
 var cowsay = require('cowsay');
 request({
-  url: 'http://subfusion.net/cgi-bin/quote.pl?quote=cookie&number=1'
+  url: 'http://www.randomtriviagenerator.com/entertainment.php'
 }, function(e, r, b) {
   var $ = cheerio.load(b);
+  var row = $('table tr').eq(1).find('[align="left"]');
   var cows  = ['vader', 'moofasa', 'default', 'small', 'moose', 'daemon', 'dragon'];
   var eyes  = ['XX', '@@', '><', 'Oo', '^^'];
+
+  if (e || r.statusCode !== 200 || !row.length ) {
+    question = 'What happened to your request for facts?'
+    answer = 'It failed';
+  } else {
+    question = row.first().text();
+    answer = row.eq(1).text();
+  }
+
   console.log('Prophet Moohammad says:\n', cowsay.say({
     e: eyes[Math.floor(Math.random() * eyes.length)],
     f: cows[Math.floor(Math.random() * cows.length)],
-    text: $('body').text().replace(/\"/g, '')
+    text: question + '\n\n' + answer
   }));
 });
